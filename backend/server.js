5271c612488dc16263ccd8c1a42096900ed8bd0a
Dimitar Papalazov <dimitar.papalazov@students.finki.ukim.mk>
2022-09-25 19:38:32 +0200

    Add routers in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 import express from 'express'
 import userRouter from './routes/userRouter.js'
+import exerciseRouter from './routes/exerciseRouter.js'
+import planRouter from './routes/planRouter.js'
+import progressRouter from './routes/progressRouter.js'
+import workoutRouter from './routes/workoutRouter.js'
 
 const app = express()
 const port = 3000
@@ -9,4 +13,8 @@ app.use(express.urlencoded({ extended: true }))
 app.listen(port, () => { 
   console.log(`Server started at http://localhost:${port}`)
   app.use('/users', userRouter)
+  app.use('/exercises', exerciseRouter)
+  app.use('/plans', planRouter)
+  app.use('/progresses', progressRouter)
+  app.use('/workouts', workoutRouter)
  })
