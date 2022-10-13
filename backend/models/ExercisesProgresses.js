eb9c86f043c5ddb4740c1c8119cef190eb8ba983
Dimitar Papalazov <dimitar.papalazov@students.finki.ukim.mk>
2022-10-13 21:28:49 +0200

    Fix possible errors

--- backend/models/ExercisesProgresses.js
+++ backend/models/ExercisesProgresses.js
@@ -1,7 +1,7 @@
 import Model from './Model.js'
 
 export default class ExercisesProgresses extends Model {
-  constructor(progress_id, exercise_id) {
+  constructor(exercise_id, progress_id) {
     super()
     this.progress_id = progress_id
     this.exercise_id = exercise_id
--- backend/models/ExercisesWorkouts.js
+++ backend/models/ExercisesWorkouts.js
@@ -1,7 +1,7 @@
 import Model from './Model.js'
 
 export default class ExercisesWorkouts extends Model {
-  constructor(workout_id, exercise_id) {
+  constructor(exercise_id, workout_id) {
     super()
     this.workout_id = workout_id
     this.exercise_id = exercise_id
