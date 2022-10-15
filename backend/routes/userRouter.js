1df2068a33516c5c111d2281aa5b62f85c6a17ce
Dimitar Papalazov <dimitar.papalazov@students.finki.ukim.mk>
2022-10-15 13:29:43 +0200

    Add new relation between existing data

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -4,9 +4,10 @@ import UserController from '../controllers/UserController.js'
 const userRouter = express.Router()
 const userController = new UserController()
 userRouter.post('/login', (req, res) => { userController.login(req, res) })
-// userRouter.get('/:id', (req, res) => { userController.read(req, res) })
+userRouter.get('/relations', (req, res) => { userController.relations(req, res) })
 userRouter.get('/', (req, res) => { userController.readAll(req, res) })
 userRouter.post('/', (req, res) => { userController.create(req, res) })
+userRouter.post('/addRelation', (req, res) => { userController.addRelation(req, res) })
 userRouter.delete('/:id', (req, res) => { userController.delete(req, res) })
 userRouter.put('/:id', (req, res) => { userController.update(req, res) })
 
